91896171144f8824800e8d953fb1e99459cc60e5
Wiliams Perez <skrillexwili8@gmail.com>
2020-05-25 23:23:12 -0500

    creacion de vistas de blog

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
    path('', include('core.urls')),
     path('services/', include('services.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     
 ]
