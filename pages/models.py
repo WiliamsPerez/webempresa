6fa31a30b16a799599349db2d523c4228f817feb
Wiliams Perez <skrillexwili8@gmail.com>
2020-05-26 18:25:16 -0500

    personalizando el administrador

--- pages/models.py
+++ pages/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Page(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
-    content = models.TextField(verbose_name="Contenido")
+    content = RichTextField(verbose_name="Contenido")
     order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
