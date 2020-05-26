c1a6dfc3848e47f5aecd7aabb5c4442f77675ff2
Wiliams Perez <skrillexwili8@gmail.com>
2020-05-26 17:37:43 -0500

    ordenacion y edicion directa de paginas

--- pages/models.py
+++ pages/models.py
@@ -1,19 +1,16 @@
 from django.db import models
 
 class Page(models.Model):
-    title = models.CharField(max_length=200, 
-        verbose_name="Título")
-    content = models.TextField(
-        verbose_name="Contenido")
-    created = models.DateTimeField(auto_now_add=True, 
-        verbose_name="Fecha de creación")
-    updated = models.DateTimeField(auto_now=True, 
-        verbose_name="Fecha de edición")
+    title = models.CharField(max_length=200, verbose_name="Título")
+    content = models.TextField(verbose_name="Contenido")
+    order = models.SmallIntegerField(verbose_name="Orden", default=0)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
 
     class Meta:
         verbose_name = "página"
         verbose_name_plural = "páginas"
-        ordering = ['title']
+        ordering = ['order','title']
 
     def __str__(self):
         return self.title
